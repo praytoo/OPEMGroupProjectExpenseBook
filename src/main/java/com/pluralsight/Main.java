fad3c6290e846e1e33bf31e9a480de96e85097a0
Student <oscar.lopez102004.ol@gmail.com>
2026-01-16 09:51:00 -0800

    new commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<Transaction> Transactions = TransactionFileManager.loadTransactions();  //This line loads all saved transactions from the file into memory.
-        Menu.homeScreen(Transactions);  //This line starts the main menu of your program.
+        Menu.homeScreen(Transactions);  //This line starts the main menu of your program./
+        //nw-e
     }
 }
